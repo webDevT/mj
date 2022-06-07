d0eed35176fe82c87c1c2cf5124b276e592f99a4
karinadesyatnik <karinadesyatnik27@gmail.com>
2022-06-07 16:23:43 +0300

    home

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,10 +1,68 @@
 $(function(){
 
+    //start mobile menu
     $('.menu__btn').click(function(){
         $(this).toggleClass('active');
         $('.left-sidebar').toggleClass('active');
         $('.content-wrapper').toggleClass('active');
         $('.header').toggleClass('active');
     })
+    //end mobile menu
+
+    //start slider init
+    $('.slider').slick({
+        slidesToShow: 4,
+        prevArrow: false,
+        nextArrow: false,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                    centerMode: true,
+                    
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    centerMode: true,
+                    
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    centerMode: true,
+                    
+                }
+            }
+        ]
+      });
+    //end slider init
+
+    //start tutorials slider
+    $('.tutorials__slider').slick({
+        slidesToShow: 1,
+        prevArrow: false,
+        nextArrow: false,
+        dots: true,
+        slidesToScroll: 1,
+
+      });
+
+    //end tutorials slider
+
+    //start star style
+    $('.item-star').click(function(){
+        $(this).toggleClass('active');
+    })
+
+    //end start style
+
+    
 
 });
