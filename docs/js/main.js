1c280419174679dc833b992413234588ee58cc27
karinadesyatnik <karinadesyatnik27@gmail.com>
2022-06-08 20:06:17 +0300

    home page and modals

--- docs/js/main.js
+++ docs/js/main.js
@@ -90,6 +90,7 @@ $(function(){
 
     //end start style
 
+    //start full height 
     $(function() {
         $('.news__slider-item').matchHeight(
             {
@@ -100,5 +101,63 @@ $(function(){
             }
         );
     });
+    //end full height
+
+    //start hide-show password
+    $('body').on('click', '.password-control', function(){
+        if ($('.password-input').attr('type') == 'password'){
+            $(this).addClass('view');
+            $('.password-input').attr('type', 'text');
+        } else {
+            $(this).removeClass('view');
+            $('.password-input').attr('type', 'password');
+        }
+        return false;
+    });
+    //end hide show password
+
+    //start modal functions 
+    $('.close-button, .login-btn').click(function(){
+        $('.modal-window').fadeOut();
+    })
+    $('.login-btn').click(function(){
+        $('.modal-window--sign-up').fadeOut();
+        $('.modal-window--login').fadeIn();
+    })
+    $('.login').click(function(){
+        $('.modal-window--login').fadeIn();
+    })
+
+    $('.sign-up').click(function(){
+        $('.modal-window--sign-up').fadeIn();
+    })
+
+    $('.signup-btn').click(function(){
+        $('.modal-window--login').fadeOut();
+        $('.modal-window--sign-up').show();
+    })
+
+    $('.forgot-password').click(function(){
+        $('.modal-window--login').fadeOut();
+        $('.modal-window--forgot-password').show();
+    })
+
+    $('.button-forgot-password').click(function(){
+        $('.modal-window--forgot-password').fadeOut();
+        $('.modal-window--reset-password').show();
+    })
+
+    
+
+    $(document).mouseup(function (e){ 
+        var div = $(".modal-window .modal-contnet"); 
+        var div2 = $(".modal-window");
+        if (!div.is(e.target) 
+            && div.has(e.target).length === 0) {
+          div2.fadeOut();
+        }
+      });
+    //end modul functions
+
 
 });
